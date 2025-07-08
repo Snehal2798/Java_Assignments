567cbab30aaeea9eaf47163a4c035457cb2217e4
Snehal2798 <Snehaldesail98@gmail.com>
2025-07-08 20:25:02 +0530

    This is Desai's first commit

---
+++ Statement1.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class Statement1 {
+
+    public static void main(String[] args) {
+        float A1=20.5F;
+        float B2=21.6F;
+        if (A1>B2)
+        {
+            System.out.println("B has won the race");
+        }
+        else if (B2>A1)
+        {
+            System.out.println("A has won the race");
+        }
+        else
+        {
+            System.out.println("Its a tie");
+        }
+
+    }
+
+}
