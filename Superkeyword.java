6b564d8c5a4123a02218e5ac9a2a5fdaabd3024f
Snehal2798 <Snehaldesail98@gmail.com>
2025-11-27 19:16:01 +0530

    initial commit

---
+++ Superkeyword.java
@@ -0,0 +1,49 @@
+package Practice;
+
+import java.security.PublicKey;
+
+class Person {
+    String name;
+    int age;
+    public  Person(String name, int age){
+        this.age=age;// Riya
+        this.name=name;//25
+    }
+    public void displayInfo()
+    {
+        System.out.println("Dr. name is:"+ name);
+        System.out.println("Dr. age is :" + age);
+    }
+}
+class Doctor extends Person{
+    String specialization;
+    String hospitalname;
+
+    public  Doctor(String name, int age, String specialization, String hospitalname){
+        super(name,age);
+        this.hospitalname=hospitalname;//vadiya
+        this.specialization=specialization;//Endoc
+    }
+    public void displayInfo()
+    {
+        System.out.println("**************HOSPITAL MANAGEMENT SYSTEM*********************");
+        System.out.println("Dr. name is:"+ super.name);
+        System.out.println("Dr. age is :" +super.age);
+        super.displayInfo();
+        System.out.println("Dr's specilaization  is :" +specialization);
+        System.out.println("Dr's hospital name  is :" +hospitalname);
+        System.out.println("----------------------------------");
+    }
+}
+
+public class Superkeyword {
+    public static void main(String args[])
+    {
+        Doctor d1=new Doctor("Riya",25,"Endoscopist","Vadiya Hospital");
+        d1.displayInfo();
+        Doctor d2=new Doctor("Yuya",26,"Gynic","7 Hills Hospital");
+        d2.displayInfo();
+        Doctor d3=new Doctor("Ajay",27,"Othopedic","TATA Hospital");
+        d3.displayInfo();
+    }
+}
